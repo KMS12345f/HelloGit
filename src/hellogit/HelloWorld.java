f41efc43ebc24f2ca69b0a03653dec7c1be675bd
KMS <ddllddll3@naver.com>
2021-08-26 10:06:10 +0900

    test() 모듈 완료

--- src/hellogit/HelloWorld.java
+++ src/hellogit/HelloWorld.java
@@ -7,6 +7,12 @@ public class HelloWorld {
 		System.out.println("Heool World");
 		System.out.println("지금 git 수업 중");
 		System.out.println("집중 하세요~~~ 단 한번만 수업합니다.");
+		test();
+	}
+
+	private static void test() {
+		System.out.println("새모듈 작업 완성~~~!!!");
+		
 	}
 
 }
