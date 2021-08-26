d7e78a9e908d0f5f1565bd653ed1f19b83d16788
KMS12345f <89556625+KMS12345f@users.noreply.github.com>
2021-08-26 18:08:25 +0900

    Update HelloWorld.java

--- src/hellogit/HelloWorld.java
+++ src/hellogit/HelloWorld.java
@@ -4,7 +4,7 @@ package hellogit;
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		System.out.println("Heool World");
+		System.out.println("Hello World");
 		System.out.println("지금 git 수업 중");
 		System.out.println("집중 하세요~~~ 단 한번만 수업합니다.");
 		test();
